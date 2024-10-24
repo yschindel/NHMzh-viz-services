575b4f7de12dd90e5919cf6b2a26a70ab006b8c4
Yskert Schindel <55532939+yschindel@users.noreply.github.com>
2024-10-24 16:45:03 +0200

    refactor: use .env for port

--- viz_pbi-server/main.go
+++ viz_pbi-server/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"viz_pbi-server/api"
 )
 
 func main() {
+	port := os.Getenv("PBI_SERVER_PORT")
+	if port == "" {
+		port = "3000" // Default port if not specified
+	}
 	srv := api.NewServer()
-	http.ListenAndServe(":3000", srv)
+	http.ListenAndServe(":"+port, srv)
 }

01ea5455227597739481e17a5058abdc6ba2daa3
Yskert Schindel <55532939+yschindel@users.noreply.github.com>
2024-10-25 10:33:14 +0200

    test: lca-consumer

---
+++ integration-tests/ifc/go.mod
@@ -0,0 +1,25 @@
+module ifc
+
+go 1.23.2
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/minio/minio-go/v7 v7.0.79
+	github.com/segmentio/kafka-go v0.4.47
+)
+
+require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/rs/xid v1.6.0 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+)
--- viz_lca-consumer/server/writer.go
+++ viz_lca-consumer/server/writer.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -15,16 +16,22 @@ type Writer struct {
 }
 
 func NewWriter(uri, dbName, collectionName string) *Writer {
+	log.Printf("connecting to MongoDB: %s, db: %s, collection: %s", uri, dbName, collectionName)
+
 	clientOptions := options.Client().ApplyURI(uri)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	} else {
+		log.Printf("Connected to MongoDB")
 	}
 
 	// Check the connection
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
 		log.Fatalf("Failed to ping MongoDB: %v", err)
+	} else {
+		log.Printf("Ping to MongoDB successful")
 	}
 
 	collection := client.Database(dbName).Collection(collectionName)
@@ -35,13 +42,22 @@ func NewWriter(uri, dbName, collectionName string) *Writer {
 	}
 }
 
-func (w *Writer) InsertDocument(document interface{}) (*mongo.InsertOneResult, error) {
+func (w *Writer) InsertDoc(message LcaMessage) (*mongo.InsertOneResult, error) {
+	document, err := bson.Marshal(message)
+	if err != nil {
+		log.Printf("could not marshal document: %v", err)
+		return nil, err
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	log.Printf("inserting document...")
 	result, err := w.collection.InsertOne(ctx, document)
 	if err != nil {
 		return nil, err
+	} else {
+		log.Printf("document inserted: %s", result.InsertedID)
 	}
 
 	return result, nil

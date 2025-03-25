2eb8624bb805b7d8bbdf66e488ce6607b8b81db7
Yskert Schindel <55532939+yschindel@users.noreply.github.com>
2025-03-25 15:15:10 +0100

    cleanup unused services

--- viz_azure-localdev/nhmzh-viz-services-localdev/selectDistinctPerDrop.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    project,
-    filename,
-    timestamp,
-    COUNT(distinct id) AS distinct_id_count
-FROM [dbo].[cost_data]
-GROUP BY project, filename, timestamp
-ORDER BY project, filename, timestamp
